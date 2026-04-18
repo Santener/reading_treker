a3babbd8429acc4b455e886a8430872d25f01a3c
Santener <kulli51987@gmail.com>
2026-04-18 15:36:58 +0500

    Версия 1.0 10%

---
+++ main.py
@@ -0,0 +1,14 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    #window =
+    #window.show()
+    sys.exit(app.exec())
+
+
+
+
+
+
---
+++ ui/main_window.py
@@ -0,0 +1,7 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super.__init__()
+        self.setMinimumSize(650, 520)
