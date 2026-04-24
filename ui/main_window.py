833d0c0eb19e93d2c56dc0781752a2a7bbd7a735
Santener <kulli51987@gmail.com>
2026-04-24 11:19:37 +0500

    Версия 1.0 80%

--- ui/main_window.py
+++ ui/main_window.py
@@ -4,4 +4,5 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setMinimumSize(650, 520)
+        self.setMinimumSize(1280, 850)
+        self.setStyleSheet("QMainWindow {background-image: url(resources/main_window_background2.jpg); background-repeat: np repeat}")
