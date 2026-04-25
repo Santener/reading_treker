70a3b7eb541ffd15fb8e05e33447e2f1a13037d2
Santener <kulli51987@gmail.com>
2026-04-25 21:49:20 +0500

    Version 1.1

--- ui/title_bar.py
+++ ui/title_bar.py
@@ -1,23 +1,20 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
+
 class TitleBar(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setFixedHeight(40)
+        self.setFixedHeight(50)
 
-
-        btn_exit = QPushButton("")
+        btn_exit = QPushButton()
         btn_exit.clicked.connect(self.close_window)
         btn_exit.setFixedSize(40, 40)
-
-
 
         layout = QHBoxLayout()
         layout.addStretch()
         layout.addWidget(btn_exit)
         self.setLayout(layout)
-
 
     def close_window(self):
         self.window().close()
